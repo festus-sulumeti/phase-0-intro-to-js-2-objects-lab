b5bb956dc9adea11198c46a2a01ef9d4d2e57d22
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-16 23:18:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = { name: "festus", streetAdress: "123 kenya street"}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeCopy = { ...employee }
+
+    delete employeeCopy[key]
+    return employeeCopy
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
